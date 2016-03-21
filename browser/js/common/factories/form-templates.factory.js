d325862d7ce6d44917c5f50022f2f7df0a1f5138
khumphrey <khumphrey7890@gmail.com>
2016-03-21 18:06:46 -0400

    updates

--- browser/js/common/factories/form-templates.factory.js
+++ browser/js/common/factories/form-templates.factory.js
@@ -7,21 +7,21 @@ app.factory('FormTemplateFactory', function($window) {
 
     fetchAll: function() {
       //fetch all documents that are type formTemplate
-      return db.query('type', {
-        key: 'formTemplate',
-        include_docs: true
-      })
-      .then(function(forms) {
-          return forms.rows.map(function(row) {
-            return row.doc;
-          });
-        });
-      // return db.allDocs({ include_docs: true })
-      //   .then(function(forms) {
+      // return db.query('type', {
+      //   key: 'formTemplate',
+      //   include_docs: true
+      // })
+      // .then(function(forms) {
       //     return forms.rows.map(function(row) {
       //       return row.doc;
       //     });
-      //   })
+      //   });
+      return db.allDocs({ include_docs: true })
+        .then(function(forms) {
+          return forms.rows.map(function(row) {
+            return row.doc;
+          });
+        })
     },
     fetchOne: function(formTemplateId) {
       return db.get(formTemplateId);
--- browser/js/completed-forms/completed-forms.controllers.js
+++ browser/js/completed-forms/completed-forms.controllers.js
@@ -1,6 +1,5 @@
 app.controller('CompletedFormsListCtrl', function($scope, form) {
   $scope.form = form;
-  
 });
 
 app.controller('CompletedFormsCtrl', function($scope, forms) {
