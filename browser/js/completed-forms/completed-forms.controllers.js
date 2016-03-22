3f409310578f45a8fe1587d9ad10bafab99c3304
khumphrey <khumphrey7890@gmail.com>
2016-03-22 12:14:17 -0400

    working with completed forms

--- browser/js/completed-forms/completed-forms.controllers.js
+++ browser/js/completed-forms/completed-forms.controllers.js
@@ -1,6 +1,9 @@
 app.controller('CompletedFormsListCtrl', function($scope, forms, formTemplate) {
   $scope.completedForms = forms;
   $scope.formTemplate = formTemplate;
+
+  $scope.data = {availableOptions: [10,25,50,100]}
+
   //if we implement jquery the following will work
   // $scope.tableLength = $('#completed-forms-table tbody tr').length;
 });
