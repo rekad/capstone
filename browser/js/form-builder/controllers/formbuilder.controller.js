3b881c5e3b031b447e6280090b1430ab03d49250
Khadija Wharton <kwharton128@gmail.com>
2016-03-23 16:11:46 -0400

    added highlighting on hover to forms and changes bg color on selected element

--- browser/js/form-builder/controllers/formbuilder.controller.js
+++ browser/js/form-builder/controllers/formbuilder.controller.js
@@ -1,9 +1,19 @@
 app.controller('FormBuilder', function($scope){
 	$scope.tabSelected = 'one';
-	console.log($scope.tabSelected);
 	$scope.formElements = [{type: 'checkbox'}, {type: 'multipleChoice'}, {type: 'paragraphText'}, {type: 'dropdown'}, {type: 'lineText'}];
 	
 	$scope.placeElements = function(type) {
 		$scope.formElements.push({type: type});
+	}
+
+	$scope.title = $scope.title || 'Form Title';
+	$scope.description = $scope.description || 'This describes my form!';
+
+	$scope.selectElement = function(e) {
+		$scope.selected = e;
+	}
+
+	$scope.onHover = function(e) {
+		$scope.hovered = e;
 	}
 });
