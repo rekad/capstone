c8f46b5a52854167ee4dad8f11b15e09f762e011
Khadija Wharton <kwharton128@gmail.com>
2016-03-23 18:10:09 -0400

    added more functionality to form editor

--- browser/js/form-builder/controllers/formbuilder.controller.js
+++ browser/js/form-builder/controllers/formbuilder.controller.js
@@ -1,9 +1,11 @@
 app.controller('FormBuilder', function($scope){
 	$scope.tabSelected = 'one';
-	$scope.formElements = [{type: 'checkbox'}, {type: 'multipleChoice'}, {type: 'paragraphText'}, {type: 'dropdown'}, {type: 'lineText'}];
+	$scope.formElements = [{type: 'checkbox', id: 0, options:["option 1", "option 2", "option 3"]}, {type: 'multipleChoice', id: 1, options:["option 1", "option 2", "option 3"]}, {type: 'paragraphText', id: 2}, {type: 'dropdown', id: 3, options:["opt1", "opt2", "opt3"]}, {type: 'lineText', id: 4}];
+
 	
 	$scope.placeElements = function(type) {
 		$scope.formElements.push({type: type});
+		console.log($scope.formElements);
 	}
 
 	$scope.title = $scope.title || 'Form Title';
