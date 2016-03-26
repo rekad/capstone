66183a1a9b70034e9a192c7dcdd058d9817da1ee
John Rauschenberg <john.rauschenberg@gmail.com>
2016-03-26 18:13:39 -0400

    Have very basic chart visualization working (with fake data).

--- browser/js/analysis/analysis.controllers.js
+++ browser/js/analysis/analysis.controllers.js
@@ -1,4 +1,12 @@
 app.controller('AnalysisCtrl', function($scope, forms) {
   $scope.forms = forms;
-  console.log("forms here:", forms);
+  $scope.chartTypes = ['map', 'bar graph', 'pie chart', 'donut chart'];
+  
+  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  
 });
