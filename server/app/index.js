df3f0eab24af5a58abecba573184afdd0954ebf7
Patrick <patsneep@gmail.com>
2016-03-26 19:01:20 -0400

    setup serving service workers

--- server/app/index.js
+++ server/app/index.js
@@ -4,6 +4,14 @@ var express = require('express');
 var app = express();
 module.exports = app;
 
+var path = require('path');
+
+
+app.get('/sw.js', function(req, res) {
+    res.sendFile(path.resolve(__dirname + '/../../sw.js'));
+})
+
+
 // Pass our express application pipeline into the configuration
 // function located at server/app/configure/index.js
 require('./configure')(app);
@@ -33,9 +41,8 @@ app.get('/start', function(req, res, next) {
 	res.sendFile(__dirname + '/views/splash.html');
 })
 
-app.get('/sw.js', function(req, res) {
-    res.sendFile(__dirname + '/../../sw.js');
-})
+
+
 
 app.get('/*', function (req, res) {
     res.sendFile(app.get('indexHTMLPath'));
