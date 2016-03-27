4b7b0b3043cd98b4cf474d64bda2ab9df4eef5c2
John Rauschenberg <john.rauschenberg@gmail.com>
2016-03-26 22:43:00 -0400

    The visualizations are now working with actual fake data. Jumping up and down.

--- browser/js/analysis/analysis.controllers.js
+++ browser/js/analysis/analysis.controllers.js
@@ -1,31 +1,103 @@
 app.controller('AnalysisCtrl', function($scope, forms, CompletedFormsFactory) {
   $scope.forms = forms;
   $scope.formLabels = {};
+  $scope.values = null;
   $scope.currentForm = null;
+  $scope.labels = [];
+  $scope.data = [[]];
 
-  $scope.forms.forEach(function(form) {
-    form.formElements.forEach(function(element) {
-      if (element.label) {
-        if ($scope.formLabels[form._id]) {
-          $scope.formLabels[form._id].push(element.label);
-        } else {
-          $scope.formLabels[form._id] = [element.label];
+  $scope.scourForms = function(forms, soughtItem) {
+    var result = {};
+    forms.forEach(function(form) {
+      form.formElements.forEach(function(element) {
+        if (element[soughtItem]) {
+          if (result[form._id]) {
+            result[form._id].push(element[soughtItem]);
+          } else {
+            result[form._id] = [element[soughtItem]];
+          }
         }
-      }
+      });
     });
-  });
+    return result;
+  };
+
+  $scope.formLabels = $scope.scourForms($scope.forms, 'label');
+
+  $scope.valuesFromLabels = function(soughtLabel) {
+    CompletedFormsFactory.fetchAll($scope.currentForm)
+    .then(function(forms) {
+      var result = [];
+      forms.forEach(function(form) {
+        form.formElements.forEach(function(element) {
+          if (element.label && element.label === soughtLabel) {
+            result.push(element.value);
+            //need to add something to deal with arrays here, multiple values
+          }
+      });
+    });
+      return result;
+  }).then(function(result) {
+      console.log("values from labels", result);
+      var labelsDataObject = {};
+      $scope.labels = [];
+      $scope.data = [[]];
+      result.forEach(function(value) {
+        if (labelsDataObject[value]) {
+          labelsDataObject[value] ++;
+        } else {
+          labelsDataObject[value] = 1;
+        }
+      });
+      for (var key in labelsDataObject) {
+        $scope.labels.push(key);
+        $scope.data[0].push(labelsDataObject[key]);
+      }
+    }).then(function(result) {
+      $scope.$digest();
+    });
+  };
 
   $scope.seedLabels = function(form) {
     $scope.currentForm = form._id;
+    // $scope.graphChange();
   }; 
+
+  $scope.graphChange = function() {
+    $scope.valuesFromLabels($scope.dataOptions);
+    // if (data2) $scope.valuesFromLabels(data2);
+  };
 
   $scope.chartTypes = ['map', 'bar graph', 'pie chart', 'donut chart'];
   
-  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+  // $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  // $scope.series = ['Series A'];
+  // $scope.data = [
+  //   [28, 48, 40, 19, 86, 27, 90]
+  // ];
+
+  //   $scope.valuesFromLabels = function(soughtLabel) {
+  //   CompletedFormsFactory.fetchAll($scope.currentForm)
+  //   .then(function(forms) {
+  //     console.log(forms);
+  //     var result = {};
+  //     forms.forEach(function(form) {
+  //       form.formElements.forEach(function(element) {
+  //         if (element.label && element.label === soughtLabel) {
+  //           if (result[form._id]) {
+  //             result[form._id].push(element.value);
+  //             //need to add something to deal with arrays here, multiple values
+  //           } else {
+  //             result[form._id] = [element.value];
+  //           }
+  //         }
+  //     });
+  //   });
+  //     return result;
+  // }).then(function(result) {
+  //   $scope.values = result;
+  //   console.log("values from labels", result);
+  // });
+  // };
   
 });
