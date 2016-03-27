eff0831ed4f128f069b566650fc9db959bcb5c72
John Rauschenberg <john.rauschenberg@gmail.com>
2016-03-26 20:31:54 -0400

    Got select boxes for various types of charts and data working.

--- browser/js/analysis/analysis.controllers.js
+++ browser/js/analysis/analysis.controllers.js
@@ -1,5 +1,24 @@
-app.controller('AnalysisCtrl', function($scope, forms) {
+app.controller('AnalysisCtrl', function($scope, forms, CompletedFormsFactory) {
   $scope.forms = forms;
+  $scope.formLabels = {};
+  $scope.currentForm = null;
+
+  $scope.forms.forEach(function(form) {
+    form.formElements.forEach(function(element) {
+      if (element.label) {
+        if ($scope.formLabels[form._id]) {
+          $scope.formLabels[form._id].push(element.label);
+        } else {
+          $scope.formLabels[form._id] = [element.label];
+        }
+      }
+    });
+  });
+
+  $scope.seedLabels = function(form) {
+    $scope.currentForm = form._id;
+  }; 
+
   $scope.chartTypes = ['map', 'bar graph', 'pie chart', 'donut chart'];
   
   $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
