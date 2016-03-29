ff2ec62606c89b149e3bd7d610b7e53e4f536555
Patrick <patsneep@gmail.com>
2016-03-29 13:35:17 -0400

    changing remote to production

--- browser/js/common/factories/database.factory.js
+++ browser/js/common/factories/database.factory.js
@@ -2,7 +2,7 @@ app.factory('DatabaseFactory', function($window) {
 	var PouchDB = $window.PouchDB;
 	var localDb = new PouchDB('thekraken-test');
 	var usersDb = new PouchDB('users');
-	var remoteDb = new PouchDB('https://104.131.103.208:6984/thekraken-test', {skipSetup: true, ajax: {rejectUnauthorized: false}});
+	var remoteDb = new PouchDB('https://still-garden-49949.herokuapp.com/thekraken-test', {skipSetup: true, ajax: {rejectUnauthorized: false}});
 
 	return {
 		getLocalDb: function() {
