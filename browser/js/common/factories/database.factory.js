98d30a476b9fd74dab0a2eb00d3c6e97a161b8b7
Patrick <patsneep@gmail.com>
2016-03-29 16:34:20 -0400

    use local db for now

--- browser/js/common/factories/database.factory.js
+++ browser/js/common/factories/database.factory.js
@@ -2,7 +2,9 @@ app.factory('DatabaseFactory', function($window) {
 	var PouchDB = $window.PouchDB;
 	var localDb = new PouchDB('thekraken-test');
 	var usersDb = new PouchDB('users');
-	var remoteDb = new PouchDB('https://still-garden-49949.herokuapp.com/thekraken-test', {skipSetup: true, ajax: {rejectUnauthorized: false}});
+	// var remoteDb = new PouchDB('https://still-garden-49949.herokuapp.com/thekraken-test', {skipSetup: true, ajax: {rejectUnauthorized: false}});
+	var remoteDb = new PouchDB('http://127.0.0.1:5984/thekraken-test', {skipSetup: true, ajax: {rejectUnauthorized: false}});
+
 
 	return {
 		getLocalDb: function() {
