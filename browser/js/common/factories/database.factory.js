832d0ddc76f4fb7a38ae98b9a9cb47ce8c5d3b41
Patrick <patsneep@gmail.com>
2016-03-30 11:44:33 -0400

    change to ibm instance

--- browser/js/common/factories/database.factory.js
+++ browser/js/common/factories/database.factory.js
@@ -2,7 +2,7 @@ app.factory('DatabaseFactory', function($window) {
 	var PouchDB = $window.PouchDB;
 	var localDb = new PouchDB('thekraken-test');
 	var usersDb = new PouchDB('users');
-	var remoteDb = new PouchDB('https://still-garden-49949.herokuapp.com/thekraken-test', {skipSetup: true, ajax: {rejectUnauthorized: false}});
+	var remoteDb = new PouchDB('https://rekad.cloudant.com/thekraken-test', {skipSetup: true});
 
 	return {
 		getLocalDb: function() {
