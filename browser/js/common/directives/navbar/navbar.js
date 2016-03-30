d8bd328cc502658037975cb4283f71a9c2a1ec90
khumphrey <khumphrey7890@gmail.com>
2016-03-30 15:11:39 -0400

    updates to online offline in nav

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -2,7 +2,6 @@ app.directive('navbar', function ($rootScope, $state) {
 
     return {
         restrict: 'E',
-        scope: {},
         templateUrl: 'js/common/directives/navbar/navbar.html',
         link: function (scope) {
 
