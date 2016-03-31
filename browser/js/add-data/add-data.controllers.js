51980f3f69432e8183516cb191083a30e5279c9c
Patrick <patsneep@gmail.com>
2016-03-31 13:03:34 -0400

    also fixed add-data modal

--- browser/js/add-data/add-data.controllers.js
+++ browser/js/add-data/add-data.controllers.js
@@ -2,6 +2,12 @@ app.controller('AddDataCtrl', function($scope, forms) {
 	$scope.forms = forms;
 });
 
+app.controller('CompletedFormModalCtrl', function($scope, $uibModalInstance) {
+	$scope.close = function() {
+		$uibModalInstance.close();
+	}
+});
+
 app.controller('AddDataSubmitCtrl', function($scope, form, CompletedFormsFactory, $uibModal) {
 	$scope.form = form;
 
@@ -27,11 +33,7 @@ app.controller('AddDataSubmitCtrl', function($scope, form, CompletedFormsFactory
 			.then(function(createdForm) {
 				$uibModal.open({
 					templateUrl: '/js/add-data/add-data-success.html',
-					controller: function($scope, $uibModalInstance) {
-						$scope.close = function() {
-							$uibModalInstance.close();
-						}
-					},
+					controller: 'CompletedFormModalCtrl',
 					size: 'sm'
 				});
 				console.log('Form submitted!', createdForm);
